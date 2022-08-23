d2eab36a8d89cbb124f628453cd358dd9a41c51c
GoryachkoP <gorpashchenko@gmail.com>
2022-08-23 19:38:47 +0300

    register routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
 const app = express()
 
@@ -14,34 +17,9 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'Головна сторінка',
-    isHome: true
-  })
-})
-
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Курси',
-    isCourses: true
-  })
-})
-
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Додати курс',
-    isAdd: true
-  })
-})
-
-
-
-
-
-
-
-
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 const port = process.env.PORT || 3001
 
