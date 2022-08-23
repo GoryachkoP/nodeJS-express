ab8e7d2f04660944205d58fc08d51cf71ef268d1
GoryachkoP <gorpashchenko@gmail.com>
2022-08-23 11:04:51 +0300

    working with simple html pages

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+app.get('/', (req, res, next) => {
+  // res status 200 по дефолту в express
+  // res.status(200)
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}'`);
