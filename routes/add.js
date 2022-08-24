e4a7b540262e4f3f7508c59b1846996c9e1a4c4e
GoryachkoP <gorpashchenko@gmail.com>
2022-08-24 14:53:24 +0300

    processing form

--- routes/add.js
+++ routes/add.js
@@ -2,10 +2,16 @@ const { Router }  = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.render('index', {
+  res.render('add', {
     title: 'Додати курс',
     isAdd: true
   })
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+
+  res.redirect('/courses')
 })
 
 module.exports = router
