df68df6fb205d108e70e7c8ffe24c070b673f548
pavel_goryachko <goryachko.pavel@gmail.com>
2023-01-26 17:51:39 +0200

    edit courses

--- model/course.js
+++ model/course.js
@@ -10,9 +10,29 @@ class Course {
     this.id = uuidv4()
   }
 
-  async save () {
+  static async update (course) {
     const courses = await Course.getAll()
 
+    const index = courses.findIndex(c => c.id === course.id)
+    courses[index] = course
+
+    return new Promise ((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
+  }
+
+  async save () {
+    const courses = await Course.getAll()
     courses.push(this.toObject())
     
     return new Promise ((resolve, reject) => {
